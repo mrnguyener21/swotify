f45f3b735adb99c8d2b06bd991dff8879716391e
mrnguyener21 <victortnguyen.workemail@gmail.com>
2020-07-01 17:32:10 -0700

    designed home page

--- src/component/Home/Home.jsx
+++ src/component/Home/Home.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
+import styles from './Home.module.scss'
+import swotifyLogo from '../../images/swotifyLogo.png'
+
+import {Link} from 'react-router-dom';
 
 const Home = () => {
     return (
-        <div>
-            <h1>Home</h1>
+        <div className={styles.container}>
+            <header className={styles.header}>
+                <div className={styles.logoContainer}>
+                    <img className={styles.logo} src={swotifyLogo} alt='logo'/>
+                    <h3 className={styles.name}>SWOTIFY</h3>
+                </div>
+                <div className={styles.itemContainer}>
+                    <div className={styles.item}>DISCOVER</div>
+                    <div className={styles.item}>JOIN</div>
+                </div>
+            </header>
+            <div className={styles.mainSection}>
+                <div className={styles.introContainer}>
+                    <h1 className={styles.intro}>Welcome To Swotify</h1>
+                    <h3 className={styles.subIntro}>Listen To Your Favorite Songs With A Single Click</h3>
+                    <Link className={styles.link} to='/webplayer'>
+                        OPEN WEB BROWSER
+                    </Link>
+                    
+                </div>
+                <div className={styles.jCole}></div>
+            </div>
         </div>
     )
 }
